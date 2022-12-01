93fc13a9bc48111703f7601e499d8ec9753b856f
ray-liu233 <2621886053@qq.com>
2022-12-01 23:52:48 +0800

    feat: add relation between Member and Story

--- src/main/java/com/tg/team/entity/Member.java
+++ src/main/java/com/tg/team/entity/Member.java
@@ -1,6 +1,7 @@
 package com.tg.team.entity;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "members")
@@ -12,6 +13,11 @@ public class Member {
     private String name;
     @Column(name="member_type")
     private String memberType;
+
+    @OneToMany(targetEntity = Story.class,cascade = CascadeType.ALL,fetch = FetchType.EAGER)
+    private List<Story> stories;
+
+    public void addStory(Story story){stories.add(story);}
 
     public Member(long id, String name, String memberType) {
         this.id = id;
