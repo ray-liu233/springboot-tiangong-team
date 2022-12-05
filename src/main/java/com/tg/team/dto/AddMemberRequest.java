fd430efc6ff12429e09d5be0ec12404de75c4323
ray-liu233 <2621886053@qq.com>
2022-12-06 00:34:53 +0800

    feat: modify AddMemberRequest

--- src/main/java/com/tg/team/dto/AddMemberRequest.java
+++ src/main/java/com/tg/team/dto/AddMemberRequest.java
@@ -3,17 +3,14 @@ package com.tg.team.dto;
 public class AddMemberRequest {
     private String name;
     private String memberType;
-    private Long storyId;
 
-    public AddMemberRequest(String name, String memberType, Long storyId) {
+    public AddMemberRequest(String name, String memberType) {
         this.name = name;
         this.memberType = memberType;
-        this.storyId = storyId;
     }
 
     public AddMemberRequest() {
     }
-
 
     public String getName() {
         return name;
@@ -23,20 +20,11 @@ public class AddMemberRequest {
         return memberType;
     }
 
-
     public void setName(String name) {
         this.name = name;
     }
 
     public void setMemberType(String memberType) {
         this.memberType = memberType;
-    }
-
-    public Long getStoryId() {
-        return storyId;
-    }
-
-    public void setStoryId(Long storyId) {
-        this.storyId = storyId;
     }
 }
