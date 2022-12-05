d6af70c3f05dd0ad061d0a3101113866cf8b9feb
ray-liu233 <2621886053@qq.com>
2022-12-06 00:40:38 +0800

    feat: modify MemberServiceTest

--- src/test/java/com/tg/team/service/MemberServiceTest.java
+++ src/test/java/com/tg/team/service/MemberServiceTest.java
@@ -4,34 +4,41 @@ package com.tg.team.service;
 import com.tg.team.dto.AddMemberRequest;
 import com.tg.team.entity.Member;
 import com.tg.team.repository.MemberRepository;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+@SpringBootTest
 class MemberServiceTest {
-
-    MemberRepository memberRepository = Mockito.mock(MemberRepository.class);
-    Member member = new Member("小芳", "DEV", null);
-    MemberService memberService = new MemberService(memberRepository);
+    @MockBean
+    private MemberRepository memberRepository;
+    @Autowired
+    private MemberService memberService;
+    Member member = new Member("小芳", "DEV");
 
     @Test
     public void should_return_member_when_add_members(){
-        Member member = new Member("小芳", "DEV", null);
-        Member serviceMember = memberService.addMembers(new AddMemberRequest("小芳", "DEV", null));
-        Assertions.assertEquals(serviceMember, memberRepository.save(member));
+        when(memberRepository.save(any(Member.class))).thenReturn(member);
+        AddMemberRequest addMemberRequest=new AddMemberRequest(member.getName(),member.getMemberType());
+        Member member1= memberService.addMembers(addMemberRequest);
+        assertThat(member1.getName()).isEqualTo("小芳");
+        assertThat(member1.getMemberType()).isEqualTo("DEV");
     }
 
     @Test
     public void should_return_member_when_get_members(){
         List<Member> members=new ArrayList<>();
         members.add(member);
-        memberRepository.save(member);
-        assertThat(memberService.findMembers().equals(members));
+        when(memberRepository.findAll()).thenReturn(members);
+        assertThat(members.get(0).getName()).isEqualTo("小芳");
+        assertThat(members.get(0).getMemberType()).isEqualTo("DEV");
     }
 
 }
