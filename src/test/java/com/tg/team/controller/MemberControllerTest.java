8f7dcd77ca5ead6134b44d9df9934def765e6c52
ray-liu233 <2621886053@qq.com>
2022-12-06 00:39:36 +0800

    feat: modify StoryService

--- src/test/java/com/tg/team/controller/MemberControllerTest.java
+++ src/test/java/com/tg/team/controller/MemberControllerTest.java
@@ -1,8 +1,10 @@
 package com.tg.team.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tg.team.dto.AddMemberRequest;
+import com.tg.team.entity.Member;
 import com.tg.team.service.MemberService;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -12,42 +14,57 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
 
 @WebMvcTest(MemberController.class)
 class MemberControllerTest {
     //given
     public static final String MEMBER_REQUEST = "{\n" +
             "\"name\":\"小芳\",\n" +
-            "\"memberType\":\"QA\",\n" +
-            "\"storyId\":\"null\"\n" +
-            "}";
-
+            "\"memberType\":\"QA\"\n" +
+            "}" ;
+    private Member member;
     @Autowired
     private MockMvc mockMvc;
 
     @MockBean
     private MemberService memberService;
 
+    @BeforeEach
+    public void beforeEach(){
+        member=new Member("小芳","QA");
+    }
     @Test
     void should_return_Created_when_add_members() throws Exception {
+        //given
+        when(memberService.addMembers(any(AddMemberRequest.class))).thenReturn(member);
+
         //when
         mockMvc.perform(MockMvcRequestBuilders.post("/POST/members")
                         .content(MEMBER_REQUEST)
                         .contentType(MediaType.APPLICATION_JSON))
         //then
-                .andExpect(MockMvcResultMatchers.status().isCreated());
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name", Matchers.is("小芳")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.memberType", Matchers.is("QA")));
     }
 
     @Test
     void should_return_Ok_when_get_members() throws Exception{
-        AddMemberRequest addMemberRequest=new ObjectMapper().readValue(MEMBER_REQUEST,AddMemberRequest.class);
-        memberService.addMembers(addMemberRequest);
-        mockMvc.perform(MockMvcRequestBuilders.get("/GET/members")
+          //given
+          List<Member> members=new ArrayList<>();
+          members.add(member);
+          when(memberService.findMembers()).thenReturn(members);
+
+          mockMvc.perform(MockMvcRequestBuilders.get("/GET/members")
                         .contentType(MediaType.APPLICATION_JSON))
-                    .andExpect(MockMvcResultMatchers.status().isOk());
-//                .andExpect(MockMvcResultMatchers.jsonPath("$.id",Matchers.is(1)))
-//                .andExpect(MockMvcResultMatchers.jsonPath("$.name",Matchers.is("小芳")))
-//                .andExpect(MockMvcResultMatchers.jsonPath("$.memberType",Matchers.is("QA")))
-//                .andExpect(MockMvcResultMatchers.jsonPath("$.storyId",Matchers.is("null")));
+                    .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].name",Matchers.is("小芳")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].memberType",Matchers.is("QA")));
     }
 }
